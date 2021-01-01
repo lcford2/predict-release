cb259ee08216f00458bd2c547f9ea2b1f9eef7a8
Lucas Ford <lcford2@ncsu.edu>
2021-01-01 16:54:51 -0500

    Multi-level model is up and running.

---
+++ python_scripts/multi-level-plots.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pickle
+from sklearn.metrics import r2_score
+from IPython import embed as II
+import pathlib
+import glob
+import argparse
+
+plt.style.use("ggplot")
+sns.set_context("talk")
+
+results_dir = pathlib.Path("../results")
+multi_level_dir = results_dir / "multi-level-results"
+
+def load_results(file):
+    if not file:
+        file = query_file()
+    with open(multi_level_dir/file, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+def parse_args(plot_functions):
+    parser = argparse.ArgumentParser(
+        description="Plot results for multi-level models."
+    )
+    parser.add_argument("-f", "--file", dest="results_pickle", default=None,
+                        help="File name for results pickle. Expected to be in ../results/mixed-level-results"
+                        )
+    parser.add_argument("-p", "--plot_func", choices=plot_functions.keys(), default=None, dest="plot_func",
+                        help="Provide the name of the desired plot. If none is provided an IPython interpreter will be opened.")
+    parser.add_argument("-g", "--group_labels", action="store_true", dest="group_labels",
+                        help="Flag to include group labels on plots. Can act differently on different plots.")
+    parser.add_argument("--res_names", dest="res_names", nargs="+", default=None, 
+                        help="Specify reservoir names to only plot those reservoirs for certian plots.")
+    parser.add_argument("-F", "--forecasted", dest="forecasted", action="store_true", 
+                        help="Use forecasted data instead of simple prediction.")
+    args = parser.parse_args()
+    return args
+
+def query_file():
+    files = glob.glob((multi_level_dir/"*.pickle").as_posix())
+    for i, file in enumerate(files):
+        print(f"[{i}] {file}")
+    selection = input("Enter the number of the file you wish to use: ")
+    if not selection.isnumeric():
+        raise ValueError("Must provide an integer corresponding to your selection.")
+    elif int(selection) > len(files):
+        raise ValueError("Number provided is not valid.")
+    else:
+        file = pathlib.Path(files[int(selection)])
+        return file.name
+
+def find_plot_functions(namespace):
+    plot_functions = filter(lambda x: x[:4] == "plot", namespace)
+    plot_name_dict = {"_".join(i.split("_")[1:]):i for i in plot_functions}
+    return plot_name_dict
+
+def plot_reservoir_score(data, args):
+    if args.forecasted:
+        modeled = data["data"]["forecasted"]["Release_act"].unstack()
+    else:
+        modeled = data["data"]["predicted_act"].unstack()
+    
+    actual = data["data"]["y_test_act"].unstack()
+    groupnames = data["data"]["X_test"]["compositegroup"].unstack().values[0, :]
+    scores = pd.DataFrame(index=modeled.columns, columns=["Score", "GroupName"], dtype="float64")
+    for i, index in enumerate(scores.index):
+        score = r2_score(actual[index], modeled[index])
+        scores.loc[index, "Score"] = score
+        scores.loc[index, "GroupName"] = groupnames[i]
+    scores = scores.sort_values(by=["GroupName", "Score"])
+    groupmeans = scores.groupby("GroupName").mean()
+    fig, ax = plt.subplots(1,1)
+    scores["Score"].plot.bar(ax=ax, width=0.8)
+    ticks = ax.get_xticks()
+    if getattr(args, "group_labels", None):
+        for tick, name in zip(ticks, scores["GroupName"].values.tolist()):
+            ax.text(tick, 0.1, name, rotation=90, va="bottom", ha="center")
+    else:
+        groupmeans = scores.groupby("GroupName").mean()
+        groups = groupmeans.index.tolist()
+        groupmeans = groupmeans.values.flatten()
+        groupcount = scores.groupby("GroupName").count().cumsum().values.flatten() / modeled.shape[1]
+        last = 0
+        modif = 0.4 / modeled.shape[1]
+        for i, mean in enumerate(groupmeans):
+            ax.axhline(mean, last + modif, groupcount[i]+modif, c="b")
+            group = groups[i]
+            ax.text((last+modif + groupcount[i])*modeled.shape[1]/2, mean, group, ha="center", va="bottom")
+            last = groupcount[i]
+    ax.set_ylabel("NSE")
+    plt.subplots_adjust(
+        top=0.88,
+        bottom=0.205,
+        left=0.11,
+        right=0.9,
+        hspace=0.2,
+        wspace=0.2
+    )
+    plt.show()
+
+
+def determine_grid_size(N):
+    if N <= 3:
+        return (N,1)
+    else:
+        poss_1 = [(i, N//i) for i in range(2, int(N**0.5) + 1) if N % i == 0]
+        poss_2 = [(i, (N+1) // i) for i in range(2, int((N+1)**0.5) + 1) if (N+1) % i == 0]
+        poss = poss_1 + poss_2
+        min_index = np.argmin([sum(i) for i in poss])
+        return poss[min_index]
+
+def plot_reservoir_TS(data, args):
+    if args.forecasted:
+        modeled = data["data"]["forecasted"]["Release_act"].unstack()
+    else:
+        modeled = data["data"]["predicted_act"].unstack()
+    actual = data["data"]["y_test_act"].unstack()
+    groupnames = data["data"]["X_test"]["compositegroup"].unstack().values[0, :]
+    if args.res_names:
+        res_names = np.array(args.res_names)
+    else:
+        res_names = modele.columns
+    grid_size = determine_grid_size(res_names.size)
+    if res_names.size > 4:
+        sns.set_context("paper")
+    fig, axes = plt.subplots(*grid_size)
+    axes = axes.flatten()
+    for ax, col in zip(axes, res_names):
+        actual[col].plot(ax=ax)
+        modeled[col].plot(ax=ax)
+        ax.set_title(col)
+        
+    left_over = axes.size - res_names.size
+    if left_over > 0:
+        for ax in axes[-left_over:]:
+            ax.set_axis_off()
+    # plt.subplots_adjust(
+
+    # )
+    plt.show()
+
+if __name__ == "__main__":
+    namespace = dir()
+    plot_functions = find_plot_functions(namespace)
+    args = parse_args(plot_functions=plot_functions)
+    data = load_results(file=args.results_pickle)
+    if args.plot_func:
+        globals()[plot_functions[args.plot_func]](data, args)
+    else:
+        II()
--- python_scripts/simple_model.py
+++ python_scripts/simple_model.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from statsmodels.regression.mixed_linear_model import MixedLMParams
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 import pickle
 from helper_functions import (read_tva_data, scale_multi_level_df)
 from timing_function import time_function
+from time import perf_counter as timer
+from datetime import timedelta
 from IPython import embed as II
+import sys
 
 @time_function
 def scaledOLS(df, scaler="mine"):
@@ -78,6 +82,7 @@ def scaledOLS(df, scaler="mine"):
     print(f"Test Score  : {r2_score(y_test, preds):.4f}")
 
 def scaled_MixedEffects(df, groups, scaler="mine"):
+    filename = "-".join(groups)
     for_groups = df.loc[:,groups]
     if scaler == "mine":
         scaled_df, means, std = scale_multi_level_df(df)
@@ -98,28 +103,197 @@ def scaled_MixedEffects(df, groups, scaler="mine"):
         y_scaled = pd.Series(y_scaled.reshape(-1), index=y.index)
     
     X_scaled[groups] = for_groups
+    names = {
+        "RunOfRiver": {0: "StorageDam", 1: "RunOfRiver"},
+        "NaturalOnly": {0: "ComboFlow", 1: "NaturalFlow"},
+        "PrimaryType": {0: "FloodControl",
+                        1: "Hydropower",
+                        2: "Irrigation",
+                        3: "Navigation",
+                        4: "WaterSupply"}
+    }
+    X_scaled = change_group_names(X_scaled, groups, names)
+    X_scaled = combine_columns(X_scaled, groups, "compositegroup")
+
+    # X_scaled["NaturalOnly"] = ["Natural" if i == 1 else "Both" for i in X_scaled["NaturalOnly"]]
     
     X_test = X_scaled[X_scaled.index.get_level_values(0).year >= 2010]
     X_train = X_scaled[X_scaled.index.get_level_values(0).year < 2010]
 
     y_test = y_scaled[y_scaled.index.get_level_values(0).year >= 2010]
     y_train = y_scaled[y_scaled.index.get_level_values(0).year < 2010]
+    
+    N_time_train = X_train.index.get_level_values(0).unique().shape[0]
+    N_time_test = X_test.index.get_level_values(0).unique().shape[0]
 
-    # model = sm.MixedLM(
-    #     y_train,
-    #     sm.add_constant(X_train),
-    #     groups
-    # )
-    model = sm.OLS(y_train, X_train)
-    fit = model.fit()
-    print(fit.summary())
-    # II()
-    # fit = model.fit()
-    # II()
+    # train model
+    exog = sm.add_constant(X_train)
+    groups = exog["compositegroup"]
+
+    exog_re = exog[["Storage_pre", "Net Inflow", "Release_pre"]]
+    mexog = exog[["const"]]
+    free = MixedLMParams.from_components(fe_params=np.ones(mexog.shape[1]),
+                                         cov_re=np.eye(exog_re.shape[1]))
+    md = sm.MixedLM(y_train, mexog, groups=groups, exog_re=exog_re)
+    fit_time_1 = timer()
+    mdf = md.fit(free=free)
+    fit_time_2 = timer()
+
+    trans_time_1 = timer()
+    fitted = (mdf.fittedvalues.unstack() * std["Release"] + means["Release"]).stack()
+    y_train_act = (y_train.unstack() * std["Release"] + means["Release"]).stack()
+    y_test_act = (y_test.unstack() * std["Release"] + means["Release"]).stack()
+    trans_time_2 = timer()
+    
+    train_score = r2_score(y_train_act, fitted)
+
+    fe_coefs = mdf.params
+    re_coefs = mdf.random_effects
+    
+    # test on unseen data
+    exog = sm.add_constant(X_test)
+    groups = exog["compositegroup"]
+
+    exog_re = exog[["Storage_pre", "Net Inflow", "Release_pre", "compositegroup"]]
+    mexog = exog[["const"]]
+
+    predicted = predict_mixedLM(fe_coefs, re_coefs, mexog, exog_re)
+    forecasted = forecast_mixedLM(fe_coefs, re_coefs, mexog, exog_re, means, std)
+    predicted_act = (predicted.unstack() * std["Release"] + means["Release"]).stack()
+    test_score = r2_score(y_test_act, predicted_act)
+    print(mdf.summary())
+    # print(f"N Time Train: {N_time_train}")
+    # print(f"N Time Test : {N_time_test}")
+    print(f"Train Score : {train_score:.4f}")
+    print(f"Test Score  : {test_score:.4f}")
+
+    print("\nGroup Sizes:")
+    for (label, array) in zip(md.group_labels, md.exog_re_li):
+        length = array.shape[0]
+        nres = int(length/N_time_train)
+        print(f"\t{label}: Length = {length}, # Res = {nres}")
+    print("\nGroup Coefficients:")
+    print_coef_table(re_coefs)
+    
+    output = dict(
+        re_coefs=re_coefs,
+        fe_coefs=fe_coefs,
+        data=dict(
+            X_test=X_test,
+            y_test=y_test,
+            X_train=X_train,
+            y_train=y_train,
+            fitted=fitted,
+            y_test_act=y_test_act,
+            y_train_act=y_train_act,
+            predicted_act=predicted_act,
+            forecasted=forecasted[["Release", "Storage", "Release_act", "Storage_act"]]
+        )
+    )
+
+    with open(f"../results/multi-level-results/{filename}.pickle", "wb") as f:
+        pickle.dump(output, f, protocol=4)
+    
+
+def print_coef_table(coefs):
+    rows = list(coefs.keys())
+    columns = list(coefs[rows[0]].keys())
+    longest = max(len(i) for i in rows)
+    column_lengths = [longest + 2] + [len(i) + 2 for i in columns]
+    columns.insert(0, " ")
+    print(" ".join(f"{i:{j}}" for i,j in zip(columns, column_lengths)))
+    for row in rows:
+        to_print = [coefs[row][i] for i in columns[1:]]
+        formatted = " ".join(
+            f"{i:.3f}".center(j) for i, j in zip(to_print, column_lengths[1:]))
+        print("".join([f"{row:{longest + 2}}", formatted]))    
+
+@time_function
+def predict_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re):
+    output_df = pd.Series(index=exog_re.index, dtype="float64")
+    groups = exog_re["compositegroup"].unique()
+    re_keys = re_coefs[groups[0]].keys()
+    fe_keys = exog_fe.columns
+    for group in groups:
+        re_df = exog_re.loc[exog_re["compositegroup"] == group, re_keys]
+        index = re_df.index
+        fe_df = exog_fe.loc[index, fe_keys]
+        output_df.loc[index] = re_df.dot(re_coefs[group]) + fe_df.dot(fe_coefs[fe_keys])
+    #* It is legitimately crazy how much slower this is than what is above.
+    # for i, row in exog_re.iterrows():
+    #     group = row["compositegroup"]
+    #     coefs = re_coefs[group]
+    #     keys = list(coefs.keys())
+    #     value = (row[keys] * coefs).sum() + (exog_fe.loc[i] * fe_coefs[exog_fe.columns]).sum()
+    #     output_df[i] = value
+    return output_df
+
+@time_function
+def forecast_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re, means, std):
+    # create output data frame
+    output_df = pd.DataFrame(index=exog_re.index, 
+                             columns=[
+                                 "Release", "Storage", 
+                                 "Release_act", "Storage_act", 
+                                 "Release_pre", "Storage_pre", "Net Inflow",
+                                 "compositegroup"
+                                 ],
+                             dtype="float64")
+    # get important indexers
+    groups = exog_re["compositegroup"].unique()
+    re_keys = re_coefs[groups[0]].keys()
+    fe_keys = exog_fe.columns
+    start_date = exog_re.index.get_level_values(0)[0]
+    end_date = exog_re.index.get_level_values(0)[-1]
+    idx = pd.IndexSlice
+    output_df.loc[idx[start_date, :],
+                  "Release_pre"] = exog_re.loc[idx[start_date, :], "Release_pre"]
+    output_df.loc[idx[start_date, :], "Storage_pre"] = exog_re.loc[idx[start_date, :], "Storage_pre"]
+    output_df["Net Inflow"] = exog_re["Net Inflow"]
+    output_df["compositegroup"] = exog_re["compositegroup"]
+    resers = exog_re.index.get_level_values(1).unique()
+    coeffs = pd.DataFrame(index=resers, columns=re_keys, dtype="float64")
+    nres = resers.size
+    
+    dates = pd.date_range(start=start_date, end=end_date)
+    date = dates[0]
+    for res in resers:
+        coeffs.loc[res, re_keys] = re_coefs[exog_re.loc[(date,res),"compositegroup"]]
+
+    for date in dates:
+        exog = output_df.loc[idx[date,:]][re_keys]
+        fe_exog = exog_fe.loc[idx[date,:]][fe_keys]
+        
+        rel = exog.mul(coeffs).sum(axis=1) + fe_exog.dot(fe_coefs[fe_keys])
+
+        rel_act = rel*std["Release"]+means["Release"]
+        stor_pre_act = output_df.loc[idx[date,:]]["Storage_pre"]*std["Storage"]+means["Storage"]
+        inflow_act = output_df.loc[idx[date,:]]["Net Inflow"]*std["Net Inflow"]+means["Net Inflow"]
+        
+        storage_act = stor_pre_act + inflow_act - rel_act
+        storage = (storage_act-means["Storage"])/std["Storage"]
+        
+        output_df.loc[idx[date,:],"Release"] = rel.values
+        output_df.loc[idx[date,:],"Storage"] = storage.values
+        output_df.loc[idx[date,:],"Release_act"] = rel_act.values
+        output_df.loc[idx[date,:],"Storage_act"] = storage_act.values
+        if date != end_date:
+            output_df.loc[idx[date+timedelta(days=1),:],"Release_pre"] = rel.values
+            output_df.loc[idx[date+timedelta(days=1),:],"Storage_pre"] = storage.values
+
+    return output_df
+    
+
+def change_group_names(df, groups, names):
+    for group in groups:
+        df[group] = [names[group][i] for i in df[group]]
+    return df
+
+def combine_columns(df, columns, new_name, sep="-"):
+    df[new_name] = df[columns[0]].str.cat(df[columns[1:]], sep=sep)
+    return df
+    
 
 if __name__ == "__main__":
     df = read_tva_data()
-    II()
-    # scaled_MixedEffects(df, groups = ["NaturalOnly"])
-
-    # scaledOLS(df)
+    scaled_MixedEffects(df, groups = ["NaturalOnly", "PrimaryType"])
