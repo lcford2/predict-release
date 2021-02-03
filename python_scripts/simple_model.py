65d8d77362cbf15e49bd01bb061b0f3dad2d17c2
Lucas Ford <lcford2@ncsu.edu>
2021-02-03 12:50:30 -0500

    Major modeling changes. Include interactions, fraction terms, monthly variables, and different groupings.

--- python_scripts/simple_model.py
+++ python_scripts/simple_model.py
@@ -11,6 +11,7 @@ from timing_function import time_function
 from time import perf_counter as timer
 from datetime import timedelta
 from IPython import embed as II
+import calendar
 import sys
 
 group_names = {
@@ -22,6 +23,8 @@ group_names = {
                     3: "Navigation",
                     4: "WaterSupply"}
 }
+
+inverse_groupnames = {key: {label: idx for idx, label in value.items()} for key, value in group_names.items()}
 
 @time_function
 def scaledOLS(df, scaler="mine"):
@@ -92,12 +95,34 @@ def scaledOLS(df, scaler="mine"):
     print(f"Train Score : {r2_score(y_train, fitted):.4f}")
     print(f"Test Score  : {r2_score(y_test, preds):.4f}")
 
-def scaled_MixedEffects(df, groups, scaler="mine"):
+def scaled_MixedEffects(df, groups, filter_groups=None, scaler="mine"):
     filename = "-".join(groups)
     for_groups = df.loc[:,groups]
+    fraction_names = ["Fraction_Storage",
+                    #   "Fraction_Release", 
+                      "Fraction_Net Inflow"]
+    fractions = df.loc[:, fraction_names]
+    if filter_groups:
+        filename += "_filter"
+        for key, value in filter_groups.items():
+            df = df.loc[df[key] == inverse_groupnames[key][value]]
+            # y_scaled = y_scaled.loc[X_scaled.index]
+            filename += f"_{value}"
+    
+    
+    df["Storage_Inflow_interaction"] = df["Storage_pre"].mul(
+        df["Net Inflow"])
+    df["Storage_Release_interaction"] = df["Storage_pre"].mul(
+        df["Release_pre"])
+    df["Release_Inflow_interaction"] = df["Release_pre"].mul(
+        df["Net Inflow"])
+
     if scaler == "mine":
         scaled_df, means, std = scale_multi_level_df(df)
-        X_scaled = scaled_df.loc[:, ["Storage_pre", "Net Inflow", "Release_pre"]]
+        X_scaled = scaled_df.loc[:, ["Storage_pre", "Net Inflow", "Release_pre",
+                                     "Storage_Inflow_interaction",
+                                     "Storage_Release_interaction",
+                                     "Release_Inflow_interaction"]]
         y_scaled = scaled_df.loc[:, "Release"]
     else:
         scaler = StandardScaler()
@@ -114,27 +139,61 @@ def scaled_MixedEffects(df, groups, scaler="mine"):
         y_scaled = pd.Series(y_scaled.reshape(-1), index=y.index)
     
     X_scaled[groups] = for_groups
-    
+    X_scaled[fraction_names] = fractions
+
     X_scaled = change_group_names(X_scaled, groups, group_names)
+    
+    # these reservoirs exhibit different characteristics than their group may suggest.
+    change_names = ["Douglas", "Cherokee", "Hiwassee"]
+    for ch_name in change_names:
+        size = X_scaled[X_scaled.index.get_level_values(1) == ch_name].shape[0]
+        X_scaled.loc[X_scaled.index.get_level_values(1) == ch_name, "NaturalOnly"] = ["NaturalFlow" for i in range(size)]
+
     X_scaled = combine_columns(X_scaled, groups, "compositegroup")
 
-    # X_scaled["NaturalOnly"] = ["Natural" if i == 1 else "Both" for i in X_scaled["NaturalOnly"]]
     
-    X_test = X_scaled[X_scaled.index.get_level_values(0).year >= 2010]
-    X_train = X_scaled[X_scaled.index.get_level_values(0).year < 2010]
 
-    y_test = y_scaled[y_scaled.index.get_level_values(0).year >= 2010]
-    y_train = y_scaled[y_scaled.index.get_level_values(0).year < 2010]
+    #* This lets me group by month 
+    # X_scaled["compositegroup"] = [calendar.month_abbr[i.month]
+    #                      for i in X_scaled.index.get_level_values(0)]
+
+    #* this introduces a intercept that varies monthly and between groups
+    month_arrays = {i:[] for i in calendar.month_abbr[1:]}
+    for date in X_scaled.index.get_level_values(0):
+        for key in month_arrays.keys():
+            if calendar.month_abbr[date.month] == key:
+                month_arrays[key].append(1)
+            else:
+                month_arrays[key].append(0)
+
+    for key, array in month_arrays.items():
+        X_scaled[key] = array
+    
+    X_test = X_scaled.loc[X_scaled.index.get_level_values(0).year >= 2010]
+    X_train = X_scaled.loc[X_scaled.index.get_level_values(0).year < 2010]
+
+    y_test = y_scaled.loc[y_scaled.index.get_level_values(0).year >= 2010]
+    y_train = y_scaled.loc[y_scaled.index.get_level_values(0).year < 2010]
     
     N_time_train = X_train.index.get_level_values(0).unique().shape[0]
     N_time_test = X_test.index.get_level_values(0).unique().shape[0]
 
     # train model
+    # Instead of adding a constant, I want to create dummy variable that accounts for season differences
     exog = sm.add_constant(X_train)
+    
     groups = exog["compositegroup"]
+    # Storage Release Interactions are near Useless
+    # Release Inflow Interaction does not provide a lot either
+    # Storage Inflow interaction seems to matter for ComboFlow-StorageDam reservoirs.
+    interaction_terms = ["Storage_Inflow_interaction"]
+    
+    exog_terms = ["Storage_pre", "Net Inflow", "Release_pre"]
 
-    exog_re = exog[["Storage_pre", "Net Inflow", "Release_pre"]]
+    exog_re = exog[exog_terms + interaction_terms + calendar.month_abbr[1:]]
+
     mexog = exog[["const"]]
+
     free = MixedLMParams.from_components(fe_params=np.ones(mexog.shape[1]),
                                          cov_re=np.eye(exog_re.shape[1]))
     md = sm.MixedLM(y_train, mexog, groups=groups, exog_re=exog_re)
@@ -152,16 +211,17 @@ def scaled_MixedEffects(df, groups, scaler="mine"):
 
     fe_coefs = mdf.params
     re_coefs = mdf.random_effects
+
     
     # test on unseen data
     exog = sm.add_constant(X_test)
     groups = exog["compositegroup"]
 
-    exog_re = exog[["Storage_pre", "Net Inflow", "Release_pre", "compositegroup"]]
+    exog_re = exog[exog_terms + interaction_terms + calendar.month_abbr[1:] + ["compositegroup"]]
     mexog = exog[["const"]]
 
-    predicted = predict_mixedLM(fe_coefs, re_coefs, mexog, exog_re)
-    forecasted = forecast_mixedLM(fe_coefs, re_coefs, mexog, exog_re, means, std)
+    predicted = predict_mixedLM(fe_coefs, re_coefs, mexog, exog_re, "compositegroup")
+    forecasted = forecast_mixedLM(fe_coefs, re_coefs, mexog, exog_re, means, std, "compositegroup")
     predicted_act = (predicted.unstack() * std["Release"] + means["Release"]).stack()
     test_score = r2_score(y_test_act, predicted_act)
     forecast_score = r2_score(y_test_act, forecasted["Release_act"])
@@ -179,6 +239,7 @@ def scaled_MixedEffects(df, groups, scaler="mine"):
         print(f"\t{label}: Length = {length}, # Res = {nres}")
     print("\nGroup Coefficients:")
     print_coef_table(re_coefs)
+    # II()
     
     output = dict(
         re_coefs=re_coefs,
@@ -196,7 +257,7 @@ def scaled_MixedEffects(df, groups, scaler="mine"):
         )
     )
 
-    with open(f"../results/multi-level-results/{filename}.pickle", "wb") as f:
+    with open(f"../results/multi-level-results/with_grouped_month_vars/{filename}_SIx_pre_std_swapped_res.pickle", "wb") as f:
         pickle.dump(output, f, protocol=4)
     
 
@@ -214,13 +275,13 @@ def print_coef_table(coefs):
         print("".join([f"{row:{longest + 2}}", formatted]))    
 
 @time_function
-def predict_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re):
+def predict_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re, group_col):
     output_df = pd.Series(index=exog_re.index, dtype="float64")
-    groups = exog_re["compositegroup"].unique()
+    groups = exog_re[group_col].unique()
     re_keys = re_coefs[groups[0]].keys()
     fe_keys = exog_fe.columns
     for group in groups:
-        re_df = exog_re.loc[exog_re["compositegroup"] == group, re_keys]
+        re_df = exog_re.loc[exog_re[group_col] == group, re_keys]
         index = re_df.index
         fe_df = exog_fe.loc[index, fe_keys]
         output_df.loc[index] = re_df.dot(re_coefs[group]) + fe_df.dot(fe_coefs[fe_keys])
@@ -234,36 +295,37 @@ def predict_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re):
     return output_df
 
 @time_function
-def forecast_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re, means, std):
+def forecast_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re, means, std, group_col):
     # create output data frame
     output_df = pd.DataFrame(index=exog_re.index, 
-                             columns=[
-                                 "Release", "Storage", 
-                                 "Release_act", "Storage_act", 
-                                 "Release_pre", "Storage_pre", "Net Inflow",
-                                 "compositegroup"
-                                 ],
+                             columns=list(exog_re.columns) + ["Storage_act", "Release_act"],
                              dtype="float64")
     # get important indexers
-    groups = exog_re["compositegroup"].unique()
+    groups = exog_re[group_col].unique()
     re_keys = re_coefs[groups[0]].keys()
     fe_keys = exog_fe.columns
     start_date = exog_re.index.get_level_values(0)[0]
     end_date = exog_re.index.get_level_values(0)[-1]
+
     idx = pd.IndexSlice
     output_df.loc[idx[start_date, :],
                   "Release_pre"] = exog_re.loc[idx[start_date, :], "Release_pre"]
     output_df.loc[idx[start_date, :], "Storage_pre"] = exog_re.loc[idx[start_date, :], "Storage_pre"]
-    output_df["Net Inflow"] = exog_re["Net Inflow"]
-    output_df["compositegroup"] = exog_re["compositegroup"]
+
+    for col in output_df.columns:
+        if col not in ["Release_pre", "Storage_pre", "Storage_act", "Release_act"]:
+            output_df[col] = exog_re.loc[:,col]
+
+    output_df[group_col] = exog_re[group_col]
     resers = exog_re.index.get_level_values(1).unique()
     coeffs = pd.DataFrame(index=resers, columns=re_keys, dtype="float64")
     nres = resers.size
-    
+
     dates = pd.date_range(start=start_date, end=end_date)
     date = dates[0]
+
     for res in resers:
-        coeffs.loc[res, re_keys] = re_coefs[exog_re.loc[(date,res),"compositegroup"]]
+        coeffs.loc[res, re_keys] = re_coefs[exog_re.loc[(date,res),group_col]]
 
     for date in dates:
         exog = output_df.loc[idx[date,:]][re_keys]
@@ -286,6 +348,7 @@ def forecast_mixedLM(fe_coefs, re_coefs, exog_fe, exog_re, means, std):
             output_df.loc[idx[date+timedelta(days=1),:],"Release_pre"] = rel.values
             output_df.loc[idx[date+timedelta(days=1),:],"Storage_pre"] = storage.values
 
+    output_df = output_df.drop(calendar.month_abbr[1:], axis=1)
     return output_df
 
 def change_group_names(df, groups, names):
@@ -344,9 +407,10 @@ def forecast_mixedLM_other_res(groups, unseen=True):
     
 
 if __name__ == "__main__":
-    # df = read_tva_data()
-    forecast_mixedLM_other_res(groups=["NaturalOnly", "RunOfRiver"])
-    # scaled_MixedEffects(df, groups = ["NaturalOnly","RunOfRiver"])
+    df = read_tva_data()
+    # forecast_mixedLM_other_res(groups=["NaturalOnly", "RunOfRiver"])
+    scaled_MixedEffects(df, groups = ["NaturalOnly","RunOfRiver"])
+                        # filter_groups={"NaturalOnly":"ComboFlow"})
     # Recorded Forecast Scores:
     # NaturalOnly, PrimaryType :             0.9643
     # NaturalOnly, RunOfRiver :              0.9646
