85596dbfab1c2f934b10a89b3b9e3abf4fb83ff8
Lucas Ford <lcford2@ncsu.edu>
2021-06-04 08:31:20 -0400

    Made input files for graps.

--- python_scripts/graps/build_windows.bat
+++ python_scripts/graps/build_windows.bat
@@ -4,7 +4,7 @@ cd src
 mkdir build
 xcopy *.f* build
 cd build
-ifort /check:all /traceback path_vars.f90 definitions.f90 My_variables.f90 ffsqp.f qld.f all_simul.f90 init.f90 multireservoir.f90 /exe:multireservoir
+ifort /check:all /traceback path_vars.f90 calpak.f90 definitions.f90 My_variables.f90 ffsqp.f qld.f all_simul.f90 init.f90 multireservoir.f90 /exe:multireservoir
 del *.f*
 move * ..\..\bin
 cd ..\..
