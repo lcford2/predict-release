5c14613687278719da079f955ad86919bf114ca1
lcford2 <lcford2@ncsu.edu>
2021-08-09 09:36:32 -0400

    Powershell script to convert md to pdf files.

--- notes/src/convert_md_to_pdf.ps1
+++ notes/src/convert_md_to_pdf.ps1
@@ -2,15 +2,26 @@
 # Purpose: Convert markdown to pdf files using pandoc
 # Date: 08/03/2021
 
+# get user provided arg
 $md_file=$args[0]
 
+# must provide an argument, else exit gracefully and help
 if ($null -eq $md_file) {
     Write-Host "Must provide path to md file."
     Write-Host "e.g. .\convert_md_to_pdf.ps1 ..\md\example.md"
 } else {
+    # replace all 'md' with 'pdf'
+    # would be safer to only replace '.md' with '.pdf'
+    # but I know that the diretory structure I am using 
+    # has markdown files in an 'md' folder and pdf files
+    # in a 'pdf' folder so this is two birds
+    # with one stone
     $pdf_file=$md_file -replace "md", "pdf"
 
+    # update user on whats going on
     Write-Host "Converting" $md_file, "to", $pdf_file
+    # luafilter here parses the md file and replaces 
+    # {{}} fields with proper text.
     pandoc $md_file -o $pdf_file --from markdown --template eisvogel `
         --listing --lua-filter=currentdate.lua
     return
