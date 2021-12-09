b4c991190adc1dc433733734a33b20cd8a459c72
Lucas Ford <lcford2@ncsu.edu>
2021-12-09 10:48:54 -0500

    Figures for AGU

--- python_scripts/agu_2021_plots.py
+++ python_scripts/agu_2021_plots.py
@@ -13,7 +13,15 @@ plt.style.use("seaborn-paper")
 sns.set_context("paper")
 colors = sns.color_palette()
 
-TVA = read_tva_data(just_load=True)
+ncsu_colors = [
+    "#990000", "#D14905", "#FAC800", "#6F7D1C",
+    "#008473", "#427E93", "#4156A1"
+]
+my_ncsu_colors = ["#ea1500", "#84a0dc"]
+bg = "#F2F2F2"
+colors = ncsu_colors[::-1]
+
+# TVA = read_tva_data(just_load=True)
 TREE_RES = ['BlueRidge', 'Chatuge', 'Cherokee', 'Douglas', 'Fontana', 'Hiwassee',
        'Norris', 'Nottely', 'SHolston', 'TimsFord', 'Watauga']
 SIMP_RES = ['Apalachia', 'Boone', 'Chikamauga', 'FtLoudoun', 'FtPatrick',
@@ -30,6 +38,11 @@ def load_simple_data():
         results = pickle.load(f)
     return results
 
+def load_simple_all_data():
+    with open("../results/agu_2021_runs/simple_model_temporal_validation_all_res/results.pickle", "rb") as f:
+        results = pickle.load(f)
+    return results
+
 def combine_tree_simple_release(tree, simple):
     train_data = tree["data"]["train_data"]
     train_data = train_data.append(simple["data"]["y_results"]).sort_index()
@@ -39,9 +52,12 @@ def combine_tree_simple_release(tree, simple):
 
     fc_data = tree["data"]["test_f_data"]
     fc_data = fc_data.append(simple["data"]["y_forc"]).sort_index()
-    test_data = pd.DataFrame({"actual":pred_data["actual"], "preds":pred_data["model"], "fc":fc_data["model"]})
+    test_data = combine_pred_fc_data(pred_data, fc_data)
     test_data = test_data.dropna()
     return train_data, test_data
+
+def combine_pred_fc_data(pred_data, fc_data):
+    return pd.DataFrame({"actual":pred_data["actual"], "preds":pred_data["model"], "fc":fc_data["model"]})
 
 def combine_tree_simple_storage(tree, simple):
     stor = tree["data"]["forecasted"]["Storage_act"]
@@ -64,6 +80,7 @@ def plot_score_bars(test_data):
     # resers = res_grouper.unique()
     resers = np.array([*TREE_RES, *SIMP_RES])
     fig, axes = plt.subplots(1, 2, sharey=True)
+    fig.patch.set_alpha(0.0)
     axes = axes.flatten()
 
     nse_fc = calc_grouped_metric(
@@ -101,18 +118,26 @@ def plot_score_bars(test_data):
     y1 = [i - width/2 for i in range(resers.size)]
     y2 = [i + width/2 for i in range(resers.size)]
 
-    ax1.barh(y1, nse_pred.loc[resers], height=width, color=colors[0], label="P")
-    ax1.barh(y2, nse_fc.loc[resers], height=width, color=colors[1], label="S")
+    act_names = {}
+    with open("./actual_names.csv", "r") as f:
+        for line in f.readlines():
+            line = line.strip("\n\r")
+            mname, aname = line.split(",")
+            act_names[mname] = aname
+    labels = [act_names.get(i) for i in resers]
+
+    ax1.barh(y2, nse_pred.loc[resers], height=width, color=my_ncsu_colors[0], label="P")
+    ax1.barh(y1, nse_fc.loc[resers], height=width, color=my_ncsu_colors[1], label="S")
     ax1.set_yticks(range(resers.size))
-    ax1.set_yticklabels(resers)
+    ax1.set_yticklabels(labels)
     ax1.set_xlabel("Release NSE")
     ax1.grid(visible=True, axis="x")
 
     ax1 = axes[1]
     # ax2 = ax1.twiny()
 
-    ax1.barh(y1, rmse_pred.loc[resers], height=width, color=colors[0], label="Predicted")
-    ax1.barh(y2, rmse_fc.loc[resers], height=width, color=colors[1], label="Simulated")
+    ax1.barh(y2, rmse_pred.loc[resers], height=width, color=my_ncsu_colors[0], label="Predicted")
+    ax1.barh(y1, rmse_fc.loc[resers], height=width, color=my_ncsu_colors[1], label="Simulated")
     ax1.set_xlabel("Release RMSE [%]")
     ax1.grid(visible=True, axis="x")
     ax1.legend(loc="best")
@@ -143,12 +168,52 @@ def plot_time_series(test_data):
 
     plt.show()
 
+def plot_coef_bars(coefs):
+    coefs = coefs.loc[["Release_pre", "Storage_pre", "Net Inflow",
+                       "Release_roll7", "Storage_roll7", "Inflow_roll7",
+                       "Storage_Inflow_interaction"]]
 
+    coefs = coefs.rename(columns={"ComboFlow-RunOfRiver": "Run of River",
+                                  "ComboFlow-StorageDam": "LRT",
+                                  "NaturalFlow-StorageDam":"HRT"})
+    title_map = {"Run of River":"Run of River",
+                 "LRT": "Low Residence Time",
+                 "HRT": "High Residence Time"
+                 }
+    labels = [
+        "Previous Release",
+        "Previous Storage",
+        "Current Inflow",
+        "Weekly Mean Release",
+        "Weekly Mean Storage",
+        "Weekly Mean Inflow",
+        "Storage Inflow Interaction"
+    ]
+    labels = ["\n".join(i.split()) for i in labels]
+
+    fig, axes = plt.subplots(3,1,sharex=True)
+    fig.patch.set_alpha(0.0)
+    axes = axes.flatten()
+    x = range(coefs.index.size)
+    for ax, group in zip(axes, coefs.columns):
+        ax.set_prop_cycle(c=my_ncsu_colors)
+        ax.bar(x, coefs[group])
+        ax.set_title(title_map[group])
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels)
+
+    fig.text(0.02, 0.5, "Fitted Coefficient", ha="center", va="center", rotation=90)
+    plt.show()
 
 if __name__ == "__main__":
     tree = load_tree_data()
     simple = load_simple_data()
-    train_rel, test_rel = combine_tree_simple_release(tree, simple)
-    sto_data = combine_tree_simple_storage(tree, simple)
-    # plot_score_bars(test_rel)
-    plot_time_series(test_rel)
+    simple_all = load_simple_all_data()
+    sa_test_rel = combine_pred_fc_data(simple_all["data"]["y_pred"], simple_all["data"]["y_forc"]).dropna()
+    # II()
+    # sys.exit()
+    # train_rel, test_rel = combine_tree_simple_release(tree, simple)
+    # sto_data = combine_tree_simple_storage(tree, simple)
+    # plot_score_bars(sa_test_rel)
+    # plot_time_series(test_rel)
+    plot_coef_bars(simple_all["coefs"])
