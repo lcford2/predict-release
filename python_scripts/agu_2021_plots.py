02f8027c68187a32e30c17025351a641a15a2040
Lucas Ford <lcford2@ncsu.edu>
2021-12-16 10:16:20 -0500

    AGU data, scripts, and plots

--- python_scripts/agu_2021_plots.py
+++ python_scripts/agu_2021_plots.py
@@ -9,8 +9,8 @@ from IPython import embed as II
 from utils.helper_functions import read_tva_data
 from plot_helpers import determine_grid_size
 
-plt.style.use("seaborn-paper")
-sns.set_context("paper")
+plt.style.use("ggplot")
+sns.set_context("talk")
 colors = sns.color_palette()
 
 ncsu_colors = [
@@ -21,7 +21,7 @@ my_ncsu_colors = ["#ea1500", "#84a0dc"]
 bg = "#F2F2F2"
 colors = ncsu_colors[::-1]
 
-# TVA = read_tva_data(just_load=True)
+TVA = read_tva_data(just_load=True)
 TREE_RES = ['BlueRidge', 'Chatuge', 'Cherokee', 'Douglas', 'Fontana', 'Hiwassee',
        'Norris', 'Nottely', 'SHolston', 'TimsFord', 'Watauga']
 SIMP_RES = ['Apalachia', 'Boone', 'Chikamauga', 'FtLoudoun', 'FtPatrick',
@@ -40,6 +40,11 @@ def load_simple_data():
 
 def load_simple_all_data():
     with open("../results/agu_2021_runs/simple_model_temporal_validation_all_res/results.pickle", "rb") as f:
+        results = pickle.load(f)
+    return results
+
+def load_spatial_data():
+    with open("../results/agu_2021_runs/simple_model_spatial_validation_all_res_lvoneout/results.pickle", "rb") as f:
         results = pickle.load(f)
     return results
 
@@ -79,7 +84,7 @@ def plot_score_bars(test_data):
     res_grouper = test_data.index.get_level_values(1)
     # resers = res_grouper.unique()
     resers = np.array([*TREE_RES, *SIMP_RES])
-    fig, axes = plt.subplots(1, 2, sharey=True)
+    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(19,10))
     fig.patch.set_alpha(0.0)
     axes = axes.flatten()
 
@@ -126,23 +131,28 @@ def plot_score_bars(test_data):
             act_names[mname] = aname
     labels = [act_names.get(i) for i in resers]
 
-    ax1.barh(y2, nse_pred.loc[resers], height=width, color=my_ncsu_colors[0], label="P")
-    ax1.barh(y1, nse_fc.loc[resers], height=width, color=my_ncsu_colors[1], label="S")
-    ax1.set_yticks(range(resers.size))
-    ax1.set_yticklabels(labels)
-    ax1.set_xlabel("Release NSE")
-    ax1.grid(visible=True, axis="x")
+    # ax1.barh(y2, nse_pred.loc[resers], height=width, color=my_ncsu_colors[0], label="P")
+    # ax1.barh(y1, nse_fc.loc[resers], height=width, color=my_ncsu_colors[1], label="S")
+    ax1.bar(y2, nse_pred.loc[resers], width=width, label="P")
+    ax1.bar(y1, nse_fc.loc[resers], width=width, label="S")
+    ax1.set_ylabel("Release NSE")
+    ax1.grid(visible=True, axis="y")
 
     ax1 = axes[1]
     # ax2 = ax1.twiny()
 
-    ax1.barh(y2, rmse_pred.loc[resers], height=width, color=my_ncsu_colors[0], label="Predicted")
-    ax1.barh(y1, rmse_fc.loc[resers], height=width, color=my_ncsu_colors[1], label="Simulated")
-    ax1.set_xlabel("Release RMSE [%]")
-    ax1.grid(visible=True, axis="x")
+    # ax1.barh(y2, rmse_pred.loc[resers], height=width, label="Predicted")
+    # ax1.barh(y1, rmse_fc.loc[resers], height=width, label="Simulated")
+    ax1.bar(y2, rmse_pred.loc[resers], width=width, label="Predicted")
+    ax1.bar(y1, rmse_fc.loc[resers], width=width, label="Simulated")
+    ax1.set_ylabel("Release RMSE [%]")
+    ax1.grid(visible=True, axis="y")
     ax1.legend(loc="best")
+    ax1.set_xticks(range(resers.size))
+    ax1.set_xticklabels(labels, rotation=45, ha="right")
 
     plt.tight_layout()
+    # plt.savefig("/home/lford/projects/predict-release/agu_figures/nse_rmse_temp_val_bars_new.png", dpi=400, facecolor="#FFFFFFFF")
     plt.show()
 
 def plot_time_series(test_data):
@@ -205,10 +215,22 @@ def plot_coef_bars(coefs):
     fig.text(0.02, 0.5, "Fitted Coefficient", ha="center", va="center", rotation=90)
     plt.show()
 
+def plot_spatial_validation(orig_data, spatial_data):
+    spatial_scores = pd.concat([i["fc_res_scores"] for i in spatial_data.values()])
+    orig_scores = pd.DataFrame(columns=["NSE", "RMSE"], index=spatial_scores.index)
+    orig_scores["NSE"] = orig_data.groupby(
+        orig_data.index.get_level_values(1)).apply(lambda x: r2_score(x["actual"], x["fc"]))
+
+    orig_scores["RMSE"] = orig_data.groupby(
+        orig_data.index.get_level_values(1)).apply(lambda x: mean_squared_error(x["actual"], x["fc"], squared=False))
+
+    II()
+
 if __name__ == "__main__":
     tree = load_tree_data()
     simple = load_simple_data()
     simple_all = load_simple_all_data()
+    spatial_data = load_spatial_data()
     sa_test_rel = combine_pred_fc_data(simple_all["data"]["y_pred"], simple_all["data"]["y_forc"]).dropna()
     # II()
     # sys.exit()
@@ -217,3 +239,4 @@ if __name__ == "__main__":
     # plot_score_bars(sa_test_rel)
     # plot_time_series(test_rel)
     plot_coef_bars(simple_all["coefs"])
+    # plot_spatial_validation(sa_test_rel, spatial_data)
