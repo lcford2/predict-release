830426a815bdbcae3045eeaca24f088f9cabaeb7
Lucas Ford <lcford2@ncsu.edu>
2021-12-20 08:39:15 -0500

    Disabled some python checkers.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((conda-project-env-path . "sry-env"))))
+((python-mode . ((flycheck-disabled-checkers . (python-mypy python-flake8)))))
+
 ;; ((nil . ()))
