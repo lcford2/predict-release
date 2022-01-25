e4f3d7d333a8f79699681da25ed8b447f2d8a08f
Lucas Ford <lcford2@ncsu.edu>
2022-01-25 11:43:44 -0500

    Retrieved data for lower col dams

--- lower_col_data/get_ids.py
+++ lower_col_data/get_ids.py
@@ -10,21 +10,26 @@ def make_sid_query(ids):
 
 def make_url(sid_query):
     fmturl = f"https://www.usbr.gov/pn-bin/hdb/hdb.pl?svr=lchdb&sdi={sid_query}& \
-               tstp=DY&t1=1950-01-01T19:03&t2=2022-01-24T00:00&table=M&mrid=0&format=csv"
+               tstp=DY&t1=1950-01-01T19:03&t2=2022-01-24T00:00&table=R&mrid=0&format=table"
     return fmturl.replace(" ", "")
 
 def get_url_1by1(ids):
     output = []
     for sid in tqdm(ids["SID"]):
-        url = make_url(str(sid))
-        output.append(pd.read_csv(url))
+        try:
+            url = make_url(str(sid))
+            df = pd.read_csv(url)
+            output.append(df)
+        except pd.errors.ParserError:
+            print(f"ERROR: {url}")
     return output
 
 def main():
     ids = load_get_ids()
-    # sid_query = make_sid_query(ids)
-    # url = make_url(sid_query)
-    results = get_url_1by1(ids)
+    sid_query = make_sid_query(ids)
+    url = make_url(sid_query)
+    tables = pd.read_html(url)
+    df = tables[0]
     II()
 
 if __name__ == "__main__":
