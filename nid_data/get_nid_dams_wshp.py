77afc281f2ee704668ff98ace1189f2aa00a68e3
Lucas Ford <lcford2@ncsu.edu>
2022-07-01 09:10:19 -0400

    generalized script

--- nid_data/get_nid_dams_wshp.py
+++ nid_data/get_nid_dams_wshp.py
@@ -17,23 +17,23 @@ huc_buffer = gpd.GeoDataFrame(huc_gdf.copy(), geometry=huc_gdf.buffer(0.02 resol
 
 nid = pd.read_csv("./all_dams_data.csv", low_memory=False)
 nid_gdf = gpd.GeoDataFrame(nid, geometry=gpd.points_from_xy(nid["Longitude"], nid["Latitude"], crs="EPSG:4269"))
-tva_nid = gpd.tools.sjoin(nid_gdf, huc_buffer, how="inner", op="within")
+huc_nid = gpd.tools.sjoin(nid_gdf, huc_buffer, how="inner", op="within")
 
 min_size = 1
 max_size = 600
-min_value = tva_nid["Max_Storage"].min()
-max_value = tva_nid["Max_Storage"].max()
-print(tva_nid["Max_Storage"].sum())
+min_value = huc_nid["Max_Storage"].min()
+max_value = huc_nid["Max_Storage"].max()
+print(huc_nid["Max_Storage"].sum())
 
 ratio = (max_size - min_size) / (max_value - min_value)
-sizes = tva_nid["Max_Storage"] * ratio + min_size
+sizes = huc_nid["Max_Storage"] * ratio + min_size
 
 fig, ax = plt.subplots(1, 1)
 
 states.plot(ax=ax, facecolor="white", edgecolor="k")
 huc_buffer.plot(ax=ax, facecolor=(1, 1, 1, 0), edgecolor="r", alpha=1.0)
 huc_gdf.plot(ax=ax, facecolor=(1, 1, 1, 0), edgecolor="b", alpha=1.0)
-tva_nid.plot(ax=ax, markersize=sizes)
+huc_nid.plot(ax=ax, markersize=sizes)
 
 ax.set_ylim(33.95, 37.41)
 ax.set_xlim(-88.92, -80.54)
