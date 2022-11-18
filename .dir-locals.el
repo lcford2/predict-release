650e944f4b6b9d5925c0e39dd25a2275d8b855fb
Lucas Ford <lcford2@ncsu.edu>
2022-11-17 21:45:30 -0500

    update emacs dir file

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((conda-project-env-path . "sry-env"))))
-((python-mode . ((flycheck-disabled-checkers . (python-mypy python-flake8)))))
+((python-mode . ((flycheck-disabled-checkers . (python-mypy python-pylint)))))
 (with-eval-after-load 'lsp-mode
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\results\\"))
 
